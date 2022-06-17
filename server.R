2548e941635eb97effb6c07625581e7ac5f0df21
Shivani Srivastava <shivani141296@gmail.com>
2022-06-17 14:41:14 +0530

    Update server.R

--- server.R
+++ server.R
@@ -347,7 +347,7 @@ shinyServer(function(input, output,session) {
     if (is.null(input$file)|input$apply==0) {return(NULL)}
     else {
       size = dim(t(dtm_tcm()$dtm))
-      dtm_size = paste("Term Document Matrix (TDM) size is ", size[1]," X ", size[2],". Below are the first 10 docs X top 10 tokens")
+      dtm_size = paste("Document Token Matrix size is ", size[1]," X ", size[2],". Below are the first 10 docs X top 10 tokens")
       return(dtm_size)
     }
   })
